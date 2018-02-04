0f17d07dc45f635151166bb399f7c8ac3ef05e8a
ChillerDragon <chillerdragon@gmail.com>
2018-02-04 13:11:56 +0100

    added first random reply messages

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define RANDOM_MESSAGES 16
+#define MAX_MESSAGE_LEN 256
+#define RADD(INDEX, MESSAGE) strcpy(aRandomMessages[INDEX], MESSAGE);
 #define FIND(IN, OUT) if (!strcasecmp(msg, IN)){printf(OUT);return true;}
 
 bool FindMessage(const char* msg)
@@ -19,7 +22,26 @@ bool FindPattern(const char* msg)
 
 void SayRandom()
 {
-	printf("this is a random reply");
+	char aRandomMessages[RANDOM_MESSAGES][MAX_MESSAGE_LEN];
+
+	RADD(0, "this is a random reply");
+	RADD(1, "i like trains");
+	RADD(2, "this didn't sense at all");
+	RADD(3, "this isn't funny");
+	RADD(4, "wanna marry me?");
+	RADD(5, "i like bananas");
+	RADD(6, "what? xd");
+	RADD(7, "?????");
+	RADD(8, "i don't care...");
+	RADD(9, "english please");
+	RADD(10, "i didn't get you there");
+	RADD(11, "what does this even mean?");
+	RADD(12, "what are you trying to tell me?");
+	RADD(13, "technically this is illegal");
+	RADD(14, "ye?");
+	RADD(15, "wait... what?!");
+
+	printf(aRandomMessages[rand() % RANDOM_MESSAGES]);
 }
 
 void MainBrain(const char* msg)
